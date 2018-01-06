2c786741d77a5e28f16f1b2129f39ba3a3448844
Makan Azarshahy <oro1919@gmail.com>
2018-01-05 19:57:06 -0800

    Added base notes for parsing JSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,31 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  //create a holder for index and the character
+  // the idea is to loop through the JSON 'string';
+
+  let index = 0;
+  let character;
+
+  var loop = function(char) {
+  	if (char && char !== character) {
+  		throw new SyntaxError(`Expected '${char}'' instead of '${character}'`);
+  	}
+
+  	character = json.charAt(index);
+  	index++;
+  	return character;
+  }
+
+  //create a parser function for each type
+  // obj, array, string, number, special values
+
+  // create a function that moves through the loop?
+  // can I do this with a for loop?
+  // delcaring functions within a for loop isn't ideal
+
+  // create a function that calls each function depending on the character type
+  // call the parser function that goes selects the appropriate parser
+  // return that call
 };
