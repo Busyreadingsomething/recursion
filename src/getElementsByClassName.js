ffdcffa5fed49dfea278cb6d9d048800d99730b0
Makan Azarshahy <oro1919@gmail.com>
2018-01-05 19:30:20 -0800

    Added working code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  
+  let res = [];
+
+  let search = function(elem) {
+  	if(elem.classList && elem.classList.contains(className)) {
+  		res.push(elem);
+  	}
+
+  	for (let i = 0; i < elem.childNodes.length; i++) {
+  		search(elem.childNodes[i]);
+  	}
+  }
+
+  search(document.body);
+  return res;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,61 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let res = '';
+  
+  if (Array.isArray(obj)) {
+    res += '[';
+  } else if (typeof obj === 'object' && obj !== null) {
+    res += '{';
+  }
+
+  if (typeof obj === 'object' && obj !== null) {
+  	
+  	_.each(obj, function(value, i) {
+  	  if (typeof value !== 'undefined' && typeof value !== 'function') {
+  	  	if (typeof i === 'string') {
+		  res += `"${i}":`;
+		}
+
+		if (value === null) {
+		  res += 'null';        
+		} else if (typeof value === 'object') {
+		  res += stringifyJSON(value);
+		} else if (typeof value === 'string') {
+		  res += `"${value}"`;
+		} else if (typeof value !== 'undefined' && typeof value !== 'function') {
+		  res += value.toString();
+		} 
+
+		if (Array.isArray(obj)) {
+		  if (i !== obj.length - 1) {
+		    res += ',';
+		  }
+		} else if (typeof obj === 'object' && obj !== null) {
+		  let keys = Object.keys(obj);
+		  if (i !== keys[keys.length - 1]) {
+		    res += ',';
+		  }
+		}
+  	  }
+
+	});
+
+  } else {
+  	if (obj === null) {
+      res += 'null';        
+    } else if (typeof obj === 'string') {
+      res += `"${obj}"`;
+    } else if (typeof obj !== 'undefined' && typeof obj !== 'function') {
+      res += obj.toString();
+    }
+  }
+  
+  if (Array.isArray(obj)) {
+    res += ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    res += '}';
+  }
+  
+  return res;
 };
